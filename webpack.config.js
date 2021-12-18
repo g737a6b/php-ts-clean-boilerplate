c6547648f54ddd8114d35ddc97046a2f42bc4638
Hiroyuki Suzuki <github@mofg.net>
2021-12-18 12:05:56 +0900

    各configのアップデート

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,19 @@
 const path = require('path');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const ESLintPlugin = require('eslint-webpack-plugin');
 
 module.exports = {
   mode: "production", // "production" | "development" | "none"
   entry: {
-    sample: "./assets/js/main.ts"
+    'js/default': path.resolve(__dirname, 'assets/js/default.ts'),
+    'css/default': path.resolve(__dirname, 'assets/css/default.scss'),
   },
   output: {
-    path: path.resolve(__dirname, 'webroot/js'),
-    filename: '[name].js'
+    path: path.resolve(__dirname, 'webroot'),
+    filename: '[name].js',
+  },
+  resolve: {
+    extensions: ['.ts'],
   },
   module: {
     rules: [
@@ -17,11 +22,24 @@ module.exports = {
         exclude: /node_modules/,
         use: 'ts-loader',
       },
+      {
+        test: /\.scss$/,
+        exclude: /node_modules/,
+        use: [
+          // CSSファイルとJSファイルを別々にする
+          MiniCssExtractPlugin.loader,
+          // Translates CSS into CommonJS
+          "css-loader",
+          // Compiles Sass to CSS
+          "sass-loader",
+        ],
+      },
     ],
   },
   plugins: [
     new ESLintPlugin({
-      extensions: ['.ts', '.js']
-    })
+      extensions: ['ts', 'js']
+    }),
+    new MiniCssExtractPlugin(),
   ],
 };
